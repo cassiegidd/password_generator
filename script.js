212d16f74709d5c887ac4fb2e3e7cd51e33d7f5f
cassiegidd <124100826+cassiegidd@users.noreply.github.com>
2023-03-11 14:11:26 -0700

    added testing consle logs- to be removed before deployment

--- script.js
+++ script.js
@@ -14,31 +14,25 @@ function generatePassword() {
   if (userLength<8 || userLength>128) {
     var tryAgain = confirm("Oops! Be sure to choose a length between 8 & 128.");
     generatePassword();
-  }else {  
+  }
+  else {  
     var upperCase = confirm("Do you want your password to contain uppercase letters?");
     var lowerCase = confirm("Do you want your password to include lowercase letters?");
     var specialNum = confirm("Do you want your password to contain numeric and special characters?");
-    
-    for (var i = 0; i < userLength; i++) {
-      if (upperCase === true) {
-        userOptions += upperCaseOptions.charAt(Math.floor(Math.random() * upperCaseOptions.length));
-      };
-      if (lowerCase === true) {
-      userOptions += lowerCaseOptions.charAt(Math.floor(Math.random()* lowerCaseOptions.length));
-      };
-      if (specialNum === true) {
-      userOptions += specialNumOptions.charAt(Math.floor(Math.random()* specialNumOptions.length));
-      };
-  } 
-  return userOptions;
+  }
+  console.log(userLength);
+  console.log(upperCase);
+  console.log(lowerCase);
+  console.log(specialNum);
+  return userLength;
+  
 }
-}
-
 function writePassword() {
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-  }
+}
+
 
 
 // Add event listener to generate button
