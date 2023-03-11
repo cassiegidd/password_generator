d4624e4db9ac7ee73dbdf543ff7941153d014f7e
cassiegidd <124100826+cassiegidd@users.noreply.github.com>
2023-03-11 14:02:21 -0700

    added variables

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 // Assignment Code
+// make prompts for each criteria
+// store the value of each criteria input
 var generateBtn = document.querySelector("#generate");
+var userOptions = "";
+var upperCaseOptions = ["ABCDEFGHIJKLMNOPQRSTVWXYZ"];
+var lowerCaseOptions = ["abcdefghijklmnopqrstuvwxyz"];
+var specialNumOptions = ["!@#$%^&*1234567890"];
 
 
 // Write password to the #password input
 function generatePassword() {
-  var length = prompt("how many characters?");
+  var userLength = prompt("Choose the length of your password- between 8 & 128 characters");
+  if (userLength<8 || userLength>128) {
+    var tryAgain = confirm("Oops! Be sure to choose a length between 8 & 128.");
+    generatePassword();
+  }else {  
+    var upperCase = confirm("Do you want your password to contain uppercase letters?");
+    var lowerCase = confirm("Do you want your password to include lowercase letters?");
+    var specialNum = confirm("Do you want your password to contain numeric and special characters?");
+    
+    for (var i = 0; i < userLength; i++) {
+      if (upperCase === true) {
+        userOptions += upperCaseOptions.charAt(Math.floor(Math.random() * upperCaseOptions.length));
+      };
+      if (lowerCase === true) {
+      userOptions += lowerCaseOptions.charAt(Math.floor(Math.random()* lowerCaseOptions.length));
+      };
+      if (specialNum === true) {
+      userOptions += specialNumOptions.charAt(Math.floor(Math.random()* specialNumOptions.length));
+      };
+  } 
+  return userOptions;
+}
 }
 
 function writePassword() {
