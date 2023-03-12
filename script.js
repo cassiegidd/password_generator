1f3801f441120fbe988e2babbf9a93f5c70c3d43
cassiegidd <124100826+cassiegidd@users.noreply.github.com>
2023-03-11 23:05:21 -0700

    added conditionals to gather user data for userOptions

--- script.js
+++ script.js
@@ -7,30 +7,61 @@ var upperCaseOptions = ["ABCDEFGHIJKLMNOPQRSTVWXYZ"];
 var lowerCaseOptions = ["abcdefghijklmnopqrstuvwxyz"];
 var specialNumOptions = ["!@#$%^&*1234567890"];
 
-
 // Write password to the #password input
-function generatePassword() {
+function getUserInput() {
   var userLength = prompt("Choose the length of your password- between 8 & 128 characters");
   if (userLength<8 || userLength>128) {
-    var tryAgain = confirm("Oops! Be sure to choose a length between 8 & 128.");
-    generatePassword();
+    var error1 = confirm("Oops! Be sure to choose a length between 8 & 128.");
+    if (error1 === false) {
+      return;
+    }
+    else {
+      getUserInput();
+    }  
   }
   else {  
     var upperCase = confirm("Do you want your password to contain uppercase letters?");
     var lowerCase = confirm("Do you want your password to include lowercase letters?");
     var specialNum = confirm("Do you want your password to contain numeric and special characters?");
-  }
-  console.log(userLength);
-  console.log(upperCase);
-  console.log(lowerCase);
-  console.log(specialNum);
-  return userLength;
+    if (upperCase===false && lowerCase===false && specialNum===false) {
+      var error2 = confirm("Oops! Be sure to choose at least one: uppercase letters, lowercase letters, numeric/special characters");
+      if (error2 === false) {
+        return;
+      }
+      else {
+      getUserInput();
+      }
+    }    
+    else if (upperCase===true && lowerCase===true && specialNum===true) {
+      userOptions = upperCaseOptions + lowerCaseOptions + specialNumOptions;
+    }
+    else if (upperCase===false && lowerCase===true && specialNum===true) {
+      userOptions = lowerCaseOptions + specialNumOptions;
+    }
+    else if (upperCase===false && lowerCase===false && specialNum===true) {
+      userOptions = specialNumOptions;
+    }
+    else if (upperCase===true && lowerCase===true && specialNum===false) {
+      userOptions = upperCaseOptions + lowerCaseOptions;
+    }
+    else if (upperCase===true && lowerCase===false && specialNum===true) {
+      userOptions = upperCaseOptions + specialNumOptions;
+    }
+    else {
+      return;
+    }  
+  };
   
-}
+
+  //testing for values: remove before deploying
+  console.log(userLength);
+  console.log(userOptions);
+} 
 function writePassword() {
-  var password = generatePassword()
+  getUserInput();
+  var generate = generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  passwordText.value = generate;
 }
 
 
